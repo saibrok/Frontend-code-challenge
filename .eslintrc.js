1a7c9248f41caeee1776e197bbe5d02d34f665d6
Litvin Sergey <s.litvin@systtech.ru>
2021-02-16 21:28:56 +0200

    подготовил проект к работе

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+      browser: true,
+      node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+      'plugin:vue/essential',
+      'eslint:recommended',
+      '@vue/prettier',
+      'prettier',
+      'prettier/vue',
+      'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+      parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <h1>Reminds</h1>
+        <User />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import User from './components/User.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        User,
+    },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
